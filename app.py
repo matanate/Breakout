7709a45594eb093e021f77e67ec18841f9bdcc1d
Matan <matanatedgi@gmail.com>
2024-02-12 16:59:04 +0200

    change secret key to enviroment variable

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "kfndjkghdfkjn3nrkjnerijoifn9j49r9rj349otno4i"
+
+app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
 
 
 @app.route("/")
